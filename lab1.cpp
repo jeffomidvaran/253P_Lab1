6c0849409b075aa6a2a201afd83225c6cf0a4678
LovelyBuggies <ninomyemail@gmail.com>
2020-10-10 14:38:53 +0800

    feat: add creatGroups and randomShuffle

--- lab1.cpp
+++ lab1.cpp
@@ -3,6 +3,8 @@
 #include <streambuf>
 #include <vector>
 #include <optional>
+#include <unordered_set>
+#include <algorithm>
 
 using namespace std;
 
@@ -43,14 +45,55 @@ int getPerson(int startIndex, string fileText, int endIndex) {
     return fileText.find("\n", endIndexLastName);
 }
 
+void createGroups(int size, unordered_set<string> absent) {
+    vector<string>attended;
+    for(int i=0;i<students.size();i++){
+        if(absent.find(students[i])==absent.end()){
+            attended.push_back(students[i]);
+        }
+    }
+    random_shuffle(attended.begin(),attended.end());
+    int num=attended.size()/size;
+    int remain=attended.size()-num*size;
+    cout<<"groups:"<<endl;
+    int index=0;
+    for(int i=1;i<=size;i++){
+        cout<<i<<". ";
+        for(int j=0;j<num;j++){
+            cout<<attended[index]<<endl;
+            index++;
+        }
+        if(remain>0){
+            cout<<attended[index]<<endl;
+            index++;
+            remain--;
+        }
+    }
+}
+
 int main(int argc, char** argv) {
     string fileText = getFileText();
-    string studentDescriptionHeader = "Student#	Name	Email	Major	Lvl	Opt	Notes";
-    int startIndex = fileText.find(studentDescriptionHeader) + studentDescriptionHeader.length() + 2; 
+    string studentDescriptionHeader = "Student# Name    Email   Major   Lvl Opt Notes";
+    int startIndex = fileText.find(studentDescriptionHeader) + studentDescriptionHeader.length() + 2;
     int endIndex =  fileText.find("Total: ");
+    cout << startIndex << " " << endIndex << endl;
 
     do {
         startIndex = getPerson(startIndex, fileText, endIndex);
     } while(startIndex != -1);
+
+    for(string s:students){cout<<s<<" hello";}
+    cout<<"group size:";
+    int groupSize,absentNumber;
+    cin>>groupSize;
+    cout<<"absent students:";
+    cin>>absentNumber;
+    unordered_set<string>absent;
+    for(int i=0;i<absentNumber;i++){
+        string tmp;
+        getline(cin,tmp);
+        absent.insert(tmp);
+    }
+    createGroups(groupSize,absent);
     return 0;
 }
